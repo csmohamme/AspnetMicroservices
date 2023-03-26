9f9fc149ad3914c5590a5affe7f7f3f67164fd86
Mohammed Almostafa <cs.mohammedalmostafa@outlook.com>
2023-03-26 22:08:05 +0300

    GetOrdersListQuery

---
+++ src/Services/Ordering/Ordering.App/Features/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Ordering.App.Features.Queries.GetOrdersList
+{
+	public class GetOrdersListQuery : IRequest<List<OrdersVm>>
+	{
+		public string UserName { get; set; }
+
+		public GetOrdersListQuery(string userName)
+		{
+			UserName = userName;
+		}
+	}
+}
--- src/Services/Ordering/Ordering.App/Mappings/MappingProfile.cs
+++ src/Services/Ordering/Ordering.App/Mappings/MappingProfile.cs
@@ -1,8 +1,15 @@
 ﻿using AutoMapper;
+using Ordering.App.Features.Queries.GetOrdersList;
+using Ordering.Domain.Entities;
 
 namespace Ordering.App.Mappings
 {
 	public class MappingProfile : Profile
 	{
+		public MappingProfile()
+		{
+			CreateMap<Order, OrdersVm>().ReverseMap();
+
+		}
 	}
 }
