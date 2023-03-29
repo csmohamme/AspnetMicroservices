63096bfe6bd35f3368eb4a73d723acad3e4d1bf2
Mohammed Almostafa <cs.mohammedalmostafa@outlook.com>
2023-03-29 21:49:32 +0300

    DeleteOrder

---
+++ src/Services/Ordering/Ordering.App/Features/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.App.Contracts.Persistence;
+
+namespace Ordering.App.Features.Commands.DeleteOrder
+{
+	public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
+	{
+		private readonly IOrderRepository _orderRepository;
+		private readonly IMapper _mapper;
+		private readonly ILogger<DeleteOrderCommandHandler> _logger;
+
+		public DeleteOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, ILogger<DeleteOrderCommandHandler> logger)
+		{
+			_orderRepository = orderRepository;
+			_mapper = mapper;
+			_logger = logger;
+		}
+
+		public async Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+		{
+			var orderToDelete = await _orderRepository.GetByIdAsync(request.Id);
+			if (orderToDelete == null)
+			{
+				_logger.LogError("Order not exist on database.");
+			}
+			await _orderRepository.DeleteAsync(orderToDelete);
+			_logger.LogInformation($"order {orderToDelete.Id} is successfully deleted.");
+			return Unit.Value;
+		}
+	}
+}
