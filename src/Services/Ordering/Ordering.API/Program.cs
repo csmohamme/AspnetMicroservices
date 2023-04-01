d90e179d49fff86d181e0a307757d114f6c2cd6c
Mohammed Almostafa <cs.mohammedalmostafa@outlook.com>
2023-04-02 00:27:06 +0300

    Extensions file and docker

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,5 +1,7 @@
+using Ordering.API.Extensions;
 using Ordering.App;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,5 +26,11 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.MigrateDatabase<OrderContext>((context, services) =>
+{
+	var logger = services.GetService<ILogger<OrderContextSeed>>();
+	OrderContextSeed
+		.SeedAsync(context, logger)
+		.Wait();
+});
 app.Run();
